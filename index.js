6e270eacc403141528c7729b90b80d6eb502153f
mariah622 <mjustine622@gmail.com>
2022-01-09 10:58:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Your code here
+function mapToNegativize(integer) {
+    let a = []
+    for(let i = 0; i < integer.length; i++){
+        a.push(-1 * integer[i])
+    }
+    return a
+
+}
+
+function mapToNoChange(integer){
+    let a = []
+    for(let i = 0; i < integer.length; i++){
+        a.push(integer[i])
+    }
+    return a
+
+}
+
+function mapToDouble(integer){
+    let a = []
+    for(let i = 0; i< integer.length; i++){
+        a.push(2 * integer[i])
+    }
+    return a
+
+}
+
+function mapToSquare(integer) {
+    let a = []
+    for(let i = 0; i < integer.length; i++){
+        a.push(integer[i] * integer[i])
+    }
+    return a
+
+}
+
+function reduceToTotal(src, startingPoint = 0){
+    let a  = startingPoint
+
+    for(let i = 0; i < src.length; i++){
+        a = a + src[i]
+    }
+    return a 
+
+}
+
+function reduceToAllTrue(src){
+    for(let i = 0; i < src.length; i++){
+        if (!src[i]) return false 
+    }
+    return true 
+
+}
+
+
+function reduceToAnyTrue(src){
+    for(let i = 0; i < src.length; i++){
+        if (src[i]) return true
+    }
+    return false
+
+}
